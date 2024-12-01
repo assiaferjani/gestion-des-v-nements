de5a49125c9113cc6aa821e054a0e890b5024ac2
Ines55MK <ines.mf55@gmail.com>
2024-12-01 21:10:34 +0100

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from '../components/banner';
+import EventList from '../components/EventList';
+
+
+
+const Home = () => {
+  return (
+    <div>
+   <Banner />
+   <EventList />
+   
+    </div>
+  );
+};
+
+export default Home;
